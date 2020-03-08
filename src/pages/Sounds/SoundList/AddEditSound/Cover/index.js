ca6d8d55bb500ccbaf8398016f8784a9e6ece46a
proficiat <sereja.korotenko@gmail.com>
2020-03-08 12:37:19 +0200

    Retain cover quality after crop

--- src/pages/Sounds/SoundList/AddEditSound/Cover/index.js
+++ src/pages/Sounds/SoundList/AddEditSound/Cover/index.js
@@ -26,9 +26,6 @@ class Cover extends PureComponent {
       cropperdImageFile: null,
       crop: {
         aspect: 16 / 16,
-        unit: 'px',
-        width: 504,
-        height: 504,
       },
     }
 
@@ -53,36 +50,43 @@ class Cover extends PureComponent {
     const canvas = document.createElement('canvas')
     const scaleX = image.naturalWidth / image.width
     const scaleY = image.naturalHeight / image.height
-    canvas.width = crop.width
-    canvas.height = crop.height
+    const scaledCropWidth = crop.width * scaleX
+    const scaledCropHeight = crop.height * scaleY
+
+    canvas.width = Math.ceil(scaledCropWidth)
+    canvas.height = Math.ceil(scaledCropHeight)
     const ctx = canvas.getContext('2d')
 
     ctx.drawImage(
       image,
       crop.x * scaleX,
       crop.y * scaleY,
-      crop.width * scaleX,
-      crop.height * scaleY,
+      scaledCropWidth,
+      scaledCropHeight,
       0,
       0,
-      crop.width,
-      crop.height,
+      scaledCropWidth,
+      scaledCropHeight,
     )
 
     return new Promise((resolve, reject) => {
-      canvas.toBlob(blob => {
-        if (!blob) {
-          // reject(new Error('Canvas is empty'));
-          console.error('Canvas is empty')
-          return
-        }
+      canvas.toBlob(
+        blob => {
+          if (!blob) {
+            // reject(new Error('Canvas is empty'));
+            console.error('Canvas is empty')
+            return
+          }
 
-        blob.name = fileName
-        const file = new File([blob], 'fileName')
-        window.URL.revokeObjectURL(this.fileUrl)
-        this.fileUrl = window.URL.createObjectURL(blob)
-        resolve({ file, fileUrl: this.fileUrl })
-      }, 'image/jpeg')
+          blob.name = fileName
+          const file = new File([blob], 'fileName')
+          window.URL.revokeObjectURL(this.fileUrl)
+          this.fileUrl = window.URL.createObjectURL(blob)
+          resolve({ file, fileUrl: this.fileUrl })
+        },
+        'image/jpeg',
+        1,
+      )
     })
   }
 
