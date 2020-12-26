3e2b81f7b20d1db953f19566745e3595be113544
proficiat <sereja.korotenko@gmail.com>
2020-12-26 11:06:45 +0200

    Toggle Switch update (size calc)

--- src/components/ToggleSwitch/index.js
+++ src/components/ToggleSwitch/index.js
@@ -4,18 +4,21 @@ import PropTypes from 'prop-types'
 
 import styled, { css } from 'styled-components'
 
-const Slider = styled.span`
+const TOGGLE_SIZE = 22
+const INDENT = 3
+
+const Toggle = styled.span`
   position: absolute;
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
+  height: ${TOGGLE_SIZE}px;
+  width: ${TOGGLE_SIZE}px;
+  left: ${INDENT}px;
+  bottom: ${INDENT}px;
   background-color: white;
   -webkit-transition: 0.4s;
   transition: 0.4s;
 `
 
-const SlideArea = styled.span`
+const ToggleArea = styled.span`
   position: absolute;
   cursor: pointer;
   top: 0;
@@ -30,49 +33,47 @@ const SlideArea = styled.span`
     props.round &&
     css`
       border-radius: 20px;
-      ${Slider} {
+      ${Toggle} {
         border-radius: 50%;
       }
     `}
 `
 
-const Input = styled.input.attrs({
+const HiddenCheckbox = styled.input.attrs({
   type: 'checkbox',
 })`
-  &:checked + ${SlideArea} {
+  opacity: 0;
+  width: 0;
+  height: 0;
+
+  &:checked + ${ToggleArea} {
     background-color: ${props => props.theme.suicidePreventionBlue};
   }
-  &:focus + ${SlideArea} {
+  &:focus + ${ToggleArea} {
     box-shadow: 0 0 1px ${props => props.theme.suicidePreventionBlue};
   }
 
-  &:checked + ${SlideArea} > ${Slider} {
-    -webkit-transform: translateX(26px);
-    -ms-transform: translateX(26px);
-    transform: translateX(26px);
+  &:checked + ${ToggleArea} > ${Toggle} {
+    -webkit-transform: translateX(${TOGGLE_SIZE}px);
+    -ms-transform: translateX(${TOGGLE_SIZE}px);
+    transform: translateX(${TOGGLE_SIZE}px);
   }
 `
 
 const Switch = styled.label`
   position: relative;
   display: inline-block;
-  width: 60px;
-  height: 34px;
-
-  > ${Input} {
-    opacity: 0;
-    width: 0;
-    height: 0;
-  }
+  width: ${TOGGLE_SIZE * 2 + INDENT * 2}px;
+  height: ${TOGGLE_SIZE + INDENT * 2}px;
 `
 
 const ToggleSwitch = ({ isChecked, onToggle }) => {
   return (
     <Switch>
-      <Input checked={isChecked} onChange={onToggle} />
-      <SlideArea round>
-        <Slider />
-      </SlideArea>
+      <HiddenCheckbox checked={isChecked} onChange={onToggle} />
+      <ToggleArea round>
+        <Toggle />
+      </ToggleArea>
     </Switch>
   )
 }
